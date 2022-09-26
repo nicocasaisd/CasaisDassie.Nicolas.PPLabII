ecb7c64f41136af0590b4b2feef287fef24f6f6c
nicocasaisd <nicolas.casais.dassie@gmail.com>
2022-09-26 18:20:23 -0300

    Creo clases dueño y vendedor

---
+++ Entidades/TiendaElectronica.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entidades
+{
+    public static class TiendaElectronica
+    {
+        List<Vendedor> listaVendedores;
+        List<Duenio> listaDuenios;
+
+
+        
+    }
+}

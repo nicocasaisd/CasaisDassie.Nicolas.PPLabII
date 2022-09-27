0bdbe15633a0b8f26be0524fb4e3fe9572cb376d
nicocasaisd <nicolas.casais.dassie@gmail.com>
2022-09-27 17:30:37 -0300

    botones Frm Principal

--- UI/Frm_MenuPrincipal.Designer.cs
+++ UI/Frm_MenuPrincipal.Designer.cs
@@ -28,12 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_inventario = new System.Windows.Forms.Button();
+            this.btn_ventas = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_inventario
+            // 
+            this.btn_inventario.Location = new System.Drawing.Point(106, 105);
+            this.btn_inventario.Name = "btn_inventario";
+            this.btn_inventario.Size = new System.Drawing.Size(155, 120);
+            this.btn_inventario.TabIndex = 0;
+            this.btn_inventario.Text = "Inventario";
+            this.btn_inventario.UseVisualStyleBackColor = true;
+            // 
+            // btn_ventas
+            // 
+            this.btn_ventas.Location = new System.Drawing.Point(394, 105);
+            this.btn_ventas.Name = "btn_ventas";
+            this.btn_ventas.Size = new System.Drawing.Size(184, 120);
+            this.btn_ventas.TabIndex = 1;
+            this.btn_ventas.Text = "Ventas";
+            this.btn_ventas.UseVisualStyleBackColor = true;
+            // 
+            // Frm_MenuPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_ventas);
+            this.Controls.Add(this.btn_inventario);
+            this.Name = "Frm_MenuPrincipal";
             this.Text = "Frm_MenuPrincipal";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button btn_inventario;
+        private Button btn_ventas;
     }
 }

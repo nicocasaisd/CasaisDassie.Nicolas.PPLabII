5a609f7ecb78319aac9cf1c814c57b9726204691
nicocasaisd <nicolas.casais.dassie@gmail.com>
2022-09-28 13:03:23 -0300

    Arranco Frm AdminInventario

--- UI/Frm_AdminInventario.Designer.cs
+++ UI/Frm_AdminInventario.Designer.cs
@@ -28,12 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.grp_listaProductos = new System.Windows.Forms.GroupBox();
+            this.lst_listaProductos = new System.Windows.Forms.ListBox();
+            this.grp_descripcion = new System.Windows.Forms.GroupBox();
+            this.lbl_cantidad = new System.Windows.Forms.Label();
+            this.lbl_precio = new System.Windows.Forms.Label();
+            this.lbl_id = new System.Windows.Forms.Label();
+            this.grp_listaProductos.SuspendLayout();
+            this.grp_descripcion.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grp_listaProductos
+            // 
+            this.grp_listaProductos.Controls.Add(this.lst_listaProductos);
+            this.grp_listaProductos.Location = new System.Drawing.Point(35, 53);
+            this.grp_listaProductos.Name = "grp_listaProductos";
+            this.grp_listaProductos.Size = new System.Drawing.Size(236, 324);
+            this.grp_listaProductos.TabIndex = 0;
+            this.grp_listaProductos.TabStop = false;
+            this.grp_listaProductos.Text = "Lista de productos";
+            // 
+            // lst_listaProductos
+            // 
+            this.lst_listaProductos.FormattingEnabled = true;
+            this.lst_listaProductos.ItemHeight = 15;
+            this.lst_listaProductos.Location = new System.Drawing.Point(20, 22);
+            this.lst_listaProductos.Name = "lst_listaProductos";
+            this.lst_listaProductos.Size = new System.Drawing.Size(187, 214);
+            this.lst_listaProductos.TabIndex = 0;
+            // 
+            // grp_descripcion
+            // 
+            this.grp_descripcion.Controls.Add(this.lbl_id);
+            this.grp_descripcion.Controls.Add(this.lbl_precio);
+            this.grp_descripcion.Controls.Add(this.lbl_cantidad);
+            this.grp_descripcion.Location = new System.Drawing.Point(314, 53);
+            this.grp_descripcion.Name = "grp_descripcion";
+            this.grp_descripcion.Size = new System.Drawing.Size(200, 172);
+            this.grp_descripcion.TabIndex = 1;
+            this.grp_descripcion.TabStop = false;
+            this.grp_descripcion.Text = "Descripcion";
+            // 
+            // lbl_cantidad
+            // 
+            this.lbl_cantidad.AutoSize = true;
+            this.lbl_cantidad.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.lbl_cantidad.Location = new System.Drawing.Point(6, 22);
+            this.lbl_cantidad.Name = "lbl_cantidad";
+            this.lbl_cantidad.Size = new System.Drawing.Size(84, 21);
+            this.lbl_cantidad.TabIndex = 0;
+            this.lbl_cantidad.Text = "Cantidad: ";
+            // 
+            // lbl_precio
+            // 
+            this.lbl_precio.AutoSize = true;
+            this.lbl_precio.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.lbl_precio.Location = new System.Drawing.Point(25, 57);
+            this.lbl_precio.Name = "lbl_precio";
+            this.lbl_precio.Size = new System.Drawing.Size(59, 21);
+            this.lbl_precio.TabIndex = 1;
+            this.lbl_precio.Text = "Precio:";
+            // 
+            // lbl_id
+            // 
+            this.lbl_id.AutoSize = true;
+            this.lbl_id.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.lbl_id.Location = new System.Drawing.Point(49, 97);
+            this.lbl_id.Name = "lbl_id";
+            this.lbl_id.Size = new System.Drawing.Size(35, 21);
+            this.lbl_id.TabIndex = 2;
+            this.lbl_id.Text = "ID: ";
+            // 
+            // Frm_AdminInventario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grp_descripcion);
+            this.Controls.Add(this.grp_listaProductos);
+            this.Name = "Frm_AdminInventario";
             this.Text = "Frm_AdminInventario";
+            this.grp_listaProductos.ResumeLayout(false);
+            this.grp_descripcion.ResumeLayout(false);
+            this.grp_descripcion.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private GroupBox grp_listaProductos;
+        private ListBox lst_listaProductos;
+        private GroupBox grp_descripcion;
+        private Label lbl_precio;
+        private Label lbl_cantidad;
+        private Label lbl_id;
     }
 }

e6a6f604e3b18c6b995861ec67f2c1e4deab9a1c
nicocasaisd <nicolas.casais.dassie@gmail.com>
2022-09-29 16:21:54 -0300

    diseño del frm

--- UI/Frm_Venta.Designer.cs
+++ UI/Frm_Venta.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.lst_carrito = new System.Windows.Forms.ListBox();
+            this.btn_Aceptar = new System.Windows.Forms.Button();
+            this.btn_Cancelar = new System.Windows.Forms.Button();
+            this.lbl_total = new System.Windows.Forms.Label();
+            this.txt_total = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lst_carrito
@@ -37,23 +41,67 @@
             this.lst_carrito.ItemHeight = 15;
             this.lst_carrito.Location = new System.Drawing.Point(34, 163);
             this.lst_carrito.Name = "lst_carrito";
-            this.lst_carrito.Size = new System.Drawing.Size(421, 184);
+            this.lst_carrito.Size = new System.Drawing.Size(513, 184);
             this.lst_carrito.TabIndex = 0;
+            // 
+            // btn_Aceptar
+            // 
+            this.btn_Aceptar.Location = new System.Drawing.Point(391, 368);
+            this.btn_Aceptar.Name = "btn_Aceptar";
+            this.btn_Aceptar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Aceptar.TabIndex = 1;
+            this.btn_Aceptar.Text = "Aceptar";
+            this.btn_Aceptar.UseVisualStyleBackColor = true;
+            // 
+            // btn_Cancelar
+            // 
+            this.btn_Cancelar.Location = new System.Drawing.Point(472, 368);
+            this.btn_Cancelar.Name = "btn_Cancelar";
+            this.btn_Cancelar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Cancelar.TabIndex = 2;
+            this.btn_Cancelar.Text = "Cancelar";
+            this.btn_Cancelar.UseVisualStyleBackColor = true;
+            // 
+            // lbl_total
+            // 
+            this.lbl_total.AutoSize = true;
+            this.lbl_total.Location = new System.Drawing.Point(123, 369);
+            this.lbl_total.Name = "lbl_total";
+            this.lbl_total.Size = new System.Drawing.Size(38, 15);
+            this.lbl_total.TabIndex = 3;
+            this.lbl_total.Text = "label1";
+            // 
+            // txt_total
+            // 
+            this.txt_total.Enabled = false;
+            this.txt_total.Location = new System.Drawing.Point(167, 366);
+            this.txt_total.Name = "txt_total";
+            this.txt_total.Size = new System.Drawing.Size(100, 23);
+            this.txt_total.TabIndex = 4;
             // 
             // Frm_Venta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(717, 450);
+            this.Controls.Add(this.txt_total);
+            this.Controls.Add(this.lbl_total);
+            this.Controls.Add(this.btn_Cancelar);
+            this.Controls.Add(this.btn_Aceptar);
             this.Controls.Add(this.lst_carrito);
             this.Name = "Frm_Venta";
             this.Text = "Frm_Venta";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private ListBox lst_carrito;
+        private Button btn_Aceptar;
+        private Button btn_Cancelar;
+        private Label lbl_total;
+        private TextBox txt_total;
     }
 }

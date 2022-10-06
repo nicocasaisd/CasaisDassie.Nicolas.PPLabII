713e9c357d20cf34080c771e43804eefe8bf2d55
nicocasaisd <nicolas.casais.dassie@gmail.com>
2022-10-06 19:40:17 -0300

    terminado el form

--- UI/Frm_Estadisticas.Designer.cs
+++ UI/Frm_Estadisticas.Designer.cs
@@ -32,11 +32,19 @@
             this.gpb_ventasTotales = new System.Windows.Forms.GroupBox();
             this.txt_ventasTotales = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.gpb_datosCategoria = new System.Windows.Forms.GroupBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txt_productoMasVendido = new System.Windows.Forms.TextBox();
+            this.txt_promedioDeGanancia = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmb_categoria = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.gpb_totalGanancia = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_totalGanancia = new System.Windows.Forms.TextBox();
             this.gpb_ventasTotales.SuspendLayout();
-            this.groupBox1.SuspendLayout();
+            this.gpb_datosCategoria.SuspendLayout();
+            this.gpb_totalGanancia.SuspendLayout();
             this.SuspendLayout();
             // 
             // btn_Salir
@@ -78,48 +86,117 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Cant total: ";
             // 
-            // groupBox1
+            // gpb_datosCategoria
             // 
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 198);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(357, 100);
-            this.groupBox1.TabIndex = 6;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Total de Ventas";
+            this.gpb_datosCategoria.Controls.Add(this.label4);
+            this.gpb_datosCategoria.Controls.Add(this.cmb_categoria);
+            this.gpb_datosCategoria.Controls.Add(this.label3);
+            this.gpb_datosCategoria.Controls.Add(this.txt_promedioDeGanancia);
+            this.gpb_datosCategoria.Controls.Add(this.label2);
+            this.gpb_datosCategoria.Controls.Add(this.txt_productoMasVendido);
+            this.gpb_datosCategoria.Location = new System.Drawing.Point(12, 171);
+            this.gpb_datosCategoria.Name = "gpb_datosCategoria";
+            this.gpb_datosCategoria.Size = new System.Drawing.Size(357, 162);
+            this.gpb_datosCategoria.TabIndex = 6;
+            this.gpb_datosCategoria.TabStop = false;
+            this.gpb_datosCategoria.Text = "Datos por categoría";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(15, 42);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 15);
+            this.label2.Size = new System.Drawing.Size(130, 15);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Cant total: ";
+            this.label2.Text = "Producto más vendido:";
             // 
-            // textBox1
+            // txt_productoMasVendido
             // 
-            this.textBox1.Enabled = false;
-            this.textBox1.Location = new System.Drawing.Point(103, 40);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(149, 23);
-            this.textBox1.TabIndex = 0;
+            this.txt_productoMasVendido.Enabled = false;
+            this.txt_productoMasVendido.Location = new System.Drawing.Point(172, 39);
+            this.txt_productoMasVendido.Name = "txt_productoMasVendido";
+            this.txt_productoMasVendido.Size = new System.Drawing.Size(149, 23);
+            this.txt_productoMasVendido.TabIndex = 0;
+            // 
+            // txt_promedioDeGanancia
+            // 
+            this.txt_promedioDeGanancia.Enabled = false;
+            this.txt_promedioDeGanancia.Location = new System.Drawing.Point(172, 83);
+            this.txt_promedioDeGanancia.Name = "txt_promedioDeGanancia";
+            this.txt_promedioDeGanancia.Size = new System.Drawing.Size(149, 23);
+            this.txt_promedioDeGanancia.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Promedio de ganancia:";
+            // 
+            // cmb_categoria
+            // 
+            this.cmb_categoria.FormattingEnabled = true;
+            this.cmb_categoria.Location = new System.Drawing.Point(172, 126);
+            this.cmb_categoria.Name = "cmb_categoria";
+            this.cmb_categoria.Size = new System.Drawing.Size(149, 23);
+            this.cmb_categoria.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(83, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Elija categoría:";
+            // 
+            // gpb_totalGanancia
+            // 
+            this.gpb_totalGanancia.Controls.Add(this.label5);
+            this.gpb_totalGanancia.Controls.Add(this.txt_totalGanancia);
+            this.gpb_totalGanancia.Location = new System.Drawing.Point(12, 339);
+            this.gpb_totalGanancia.Name = "gpb_totalGanancia";
+            this.gpb_totalGanancia.Size = new System.Drawing.Size(357, 100);
+            this.gpb_totalGanancia.TabIndex = 6;
+            this.gpb_totalGanancia.TabStop = false;
+            this.gpb_totalGanancia.Text = "Total de Ventas";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 42);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 15);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Ganancia total: ";
+            // 
+            // txt_totalGanancia
+            // 
+            this.txt_totalGanancia.Enabled = false;
+            this.txt_totalGanancia.Location = new System.Drawing.Point(130, 39);
+            this.txt_totalGanancia.Name = "txt_totalGanancia";
+            this.txt_totalGanancia.Size = new System.Drawing.Size(149, 23);
+            this.txt_totalGanancia.TabIndex = 0;
             // 
             // Frm_Estadisticas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(536, 450);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gpb_totalGanancia);
+            this.Controls.Add(this.gpb_datosCategoria);
             this.Controls.Add(this.gpb_ventasTotales);
             this.Controls.Add(this.btn_Salir);
             this.Name = "Frm_Estadisticas";
             this.Text = "Frm_Estadisticas";
             this.gpb_ventasTotales.ResumeLayout(false);
             this.gpb_ventasTotales.PerformLayout();
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.gpb_datosCategoria.ResumeLayout(false);
+            this.gpb_datosCategoria.PerformLayout();
+            this.gpb_totalGanancia.ResumeLayout(false);
+            this.gpb_totalGanancia.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -130,8 +207,15 @@
         private GroupBox gpb_ventasTotales;
         private TextBox txt_ventasTotales;
         private Label label1;
-        private GroupBox groupBox1;
+        private GroupBox gpb_datosCategoria;
         private Label label2;
-        private TextBox textBox1;
+        private TextBox txt_productoMasVendido;
+        private Label label3;
+        private TextBox txt_promedioDeGanancia;
+        private Label label4;
+        private ComboBox cmb_categoria;
+        private GroupBox gpb_totalGanancia;
+        private Label label5;
+        private TextBox txt_totalGanancia;
     }
 }
